119f854bee17a26590579ae4e93838e2c83422a5
Harsha <harshanambur@gmail.com>
2025-03-30 13:49:25 +0530

    mobile version changes

--- script.js
+++ script.js
@@ -59,3 +59,12 @@ if (form) {
     });
   });
 }
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("hidden");
+  });
+}
